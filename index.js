7a9954ee08aba6cf5d4cc3ec175569ae4ebe7f7b
Ac-ob <157826104+Ac-ob@users.noreply.github.com>
2026-03-04 22:38:35 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,446 @@
+// Data from https://github.com/sharmadhiraj/free-json-datasets?tab=readme-ov-file  
+// DO NOT MODIFY THE DATA, start your code below 
+let myData = [
+    {
+      "language": "English(excl. creole languages)",
+      "family": "Indo-European",
+      "branch": "Germanic",
+      "first_language_millions": "390",
+      "second_language_millions": "1,138",
+      "total_millions": "1,528"
+    },
+    {
+      "language": "Mandarin Chinese(incl. Standard Chinese but excl. other varieties)",
+      "family": "Sino-Tibetan",
+      "branch": "Sinitic",
+      "first_language_millions": "990",
+      "second_language_millions": "194",
+      "total_millions": "1,184"
+    },
+    {
+      "language": "Hindi(excl. Urdu)",
+      "family": "Indo-European",
+      "branch": "Indo-Aryan",
+      "first_language_millions": "345",
+      "second_language_millions": "264",
+      "total_millions": "609"
+    },
+    {
+      "language": "Spanish(excl. creole languages)",
+      "family": "Indo-European",
+      "branch": "Romance",
+      "first_language_millions": "484",
+      "second_language_millions": "74",
+      "total_millions": "558"
+    },
+    {
+      "language": "Modern Standard Arabic(excl. dialects)",
+      "family": "Afro-Asiatic",
+      "branch": "Semitic",
+      "first_language_millions": "0",
+      "second_language_millions": "335",
+      "total_millions": "335"
+    },
+    {
+      "language": "French(excl. creole languages)",
+      "family": "Indo-European",
+      "branch": "Romance",
+      "first_language_millions": "74",
+      "second_language_millions": "238",
+      "total_millions": "312"
+    },
+    {
+      "language": "Bengali",
+      "family": "Indo-European",
+      "branch": "Indo-Aryan",
+      "first_language_millions": "242",
+      "second_language_millions": "43",
+      "total_millions": "284"
+    },
+    {
+      "language": "Portuguese(excl. creole languages)",
+      "family": "Indo-European",
+      "branch": "Romance",
+      "first_language_millions": "250",
+      "second_language_millions": "17",
+      "total_millions": "267"
+    },
+    {
+      "language": "Russian",
+      "family": "Indo-European",
+      "branch": "Balto-Slavic",
+      "first_language_millions": "145",
+      "second_language_millions": "108",
+      "total_millions": "253"
+    },
+    {
+      "language": "Indonesian",
+      "family": "Austronesian",
+      "branch": "Malayo-Polynesian",
+      "first_language_millions": "75",
+      "second_language_millions": "177",
+      "total_millions": "252"
+    },
+    {
+      "language": "Urdu(excl. Hindi)",
+      "family": "Indo-European",
+      "branch": "Indo-Aryan",
+      "first_language_millions": "78",
+      "second_language_millions": "168",
+      "total_millions": "246"
+    },
+    {
+      "language": "Standard German",
+      "family": "Indo-European",
+      "branch": "Germanic",
+      "first_language_millions": "76",
+      "second_language_millions": "58",
+      "total_millions": "134"
+    },
+    {
+      "language": "Japanese",
+      "family": "Japonic",
+      "branch": "—",
+      "first_language_millions": "124",
+      "second_language_millions": "2",
+      "total_millions": "126"
+    },
+    {
+      "language": "Nigerian Pidgin",
+      "family": "English Creole",
+      "branch": "Krio",
+      "first_language_millions": "5",
+      "second_language_millions": "116",
+      "total_millions": "121"
+    },
+    {
+      "language": "Egyptian Arabic(excl. other Arabic dialects)",
+      "family": "Afro-Asiatic",
+      "branch": "Semitic",
+      "first_language_millions": "84",
+      "second_language_millions": "35",
+      "total_millions": "119"
+    },
+    {
+      "language": "Marathi",
+      "family": "Indo-European",
+      "branch": "Indo-Aryan",
+      "first_language_millions": "83",
+      "second_language_millions": "16",
+      "total_millions": "99"
+    },
+    {
+      "language": "Vietnamese",
+      "family": "Austroasiatic",
+      "branch": "Vietic",
+      "first_language_millions": "86",
+      "second_language_millions": "11",
+      "total_millions": "97"
+    },
+    {
+      "language": "Telugu",
+      "family": "Dravidian",
+      "branch": "South-Central",
+      "first_language_millions": "83",
+      "second_language_millions": "13",
+      "total_millions": "96"
+    },
+    {
+      "language": "Hausa",
+      "family": "Afro-Asiatic",
+      "branch": "Chadic",
+      "first_language_millions": "58",
+      "second_language_millions": "36",
+      "total_millions": "94"
+    },
+    {
+      "language": "Turkish",
+      "family": "Turkic",
+      "branch": "Oghuz",
+      "first_language_millions": "85",
+      "second_language_millions": "6",
+      "total_millions": "91"
+    },
+    {
+      "language": "Western Punjabi(excl. Eastern Punjabi)",
+      "family": "Indo-European",
+      "branch": "Indo-Aryan",
+      "first_language_millions": "—",
+      "second_language_millions": "—",
+      "total_millions": "90"
+    },
+    {
+      "language": "Swahili",
+      "family": "Niger–Congo",
+      "branch": "Bantu",
+      "first_language_millions": "4",
+      "second_language_millions": "83",
+      "total_millions": "87"
+    },
+    {
+      "language": "Tagalog",
+      "family": "Austronesian",
+      "branch": "Malayo-Polynesian",
+      "first_language_millions": "33",
+      "second_language_millions": "54",
+      "total_millions": "87"
+    },
+    {
+      "language": "Tamil",
+      "family": "Dravidian",
+      "branch": "South",
+      "first_language_millions": "79",
+      "second_language_millions": "8",
+      "total_millions": "86"
+    },
+    {
+      "language": "Yue Chinese(incl. Cantonese)",
+      "family": "Sino-Tibetan",
+      "branch": "Sinitic",
+      "first_language_millions": "85",
+      "second_language_millions": "1",
+      "total_millions": "86"
+    },
+    {
+      "language": "Wu Chinese(incl. Shanghainese)",
+      "family": "Sino-Tibetan",
+      "branch": "Sinitic",
+      "first_language_millions": "83",
+      "second_language_millions": "<1",
+      "total_millions": "83"
+    },
+    {
+      "language": "Iranian Persian(excl. other Persian dialects)",
+      "family": "Indo-European",
+      "branch": "Iranian",
+      "first_language_millions": "65",
+      "second_language_millions": "17",
+      "total_millions": "83"
+    },
+    {
+      "language": "Korean",
+      "family": "Koreanic",
+      "branch": "—",
+      "first_language_millions": "81",
+      "second_language_millions": "<1",
+      "total_millions": "82"
+    },
+    {
+      "language": "Thai",
+      "family": "Kra–Dai",
+      "branch": "Zhuang–Tai",
+      "first_language_millions": "27",
+      "second_language_millions": "44",
+      "total_millions": "71"
+    },
+    {
+      "language": "Javanese",
+      "family": "Austronesian",
+      "branch": "Malayo-Polynesian",
+      "first_language_millions": "—",
+      "second_language_millions": "—",
+      "total_millions": "69"
+    },
+    {
+      "language": "Italian",
+      "family": "Indo-European",
+      "branch": "Romance",
+      "first_language_millions": "63",
+      "second_language_millions": "3",
+      "total_millions": "66"
+    },
+    {
+      "language": "Gujarati",
+      "family": "Indo-European",
+      "branch": "Indo-Aryan",
+      "first_language_millions": "58",
+      "second_language_millions": "5",
+      "total_millions": "62"
+    },
+    {
+      "language": "Levantine Arabic(excl. other Arabic dialects)",
+      "family": "Afro-Asiatic",
+      "branch": "Semitic",
+      "first_language_millions": "58",
+      "second_language_millions": "3",
+      "total_millions": "60"
+    },
+    {
+      "language": "Amharic",
+      "family": "Afro-Asiatic",
+      "branch": "Semitic",
+      "first_language_millions": "35",
+      "second_language_millions": "25",
+      "total_millions": "60"
+    },
+    {
+      "language": "Kannada",
+      "family": "Dravidian",
+      "branch": "South",
+      "first_language_millions": "44",
+      "second_language_millions": "15",
+      "total_millions": "59"
+    },
+    {
+      "language": "Bhojpuri",
+      "family": "Indo-European",
+      "branch": "Indo-Aryan",
+      "first_language_millions": "53",
+      "second_language_millions": "<1",
+      "total_millions": "53"
+    },
+    {
+      "language": "Sudanese Arabic(excl. other Arabic dialects)",
+      "family": "Afro-Asiatic",
+      "branch": "Semitic",
+      "first_language_millions": "41",
+      "second_language_millions": "11",
+      "total_millions": "52"
+    }
+  ]
+
+
+
+
+//learned to do counts with https://github.com/d3/d3-array/blob/v3.2.1/README.md#rollup
+
+let frequency_data = Array.from(d3.rollup(myData, v=> v.length, d => d.family), ([key, value]) => ({key, value}))
+
+let height = 300;
+let width = 500;
+let margin = 50;
+
+let frame = d3.select("#bar_vis")
+                .append("svg")
+                .attr("width", width + margin*2)
+                .attr("height", height + margin*2);
+
+let linearScale = d3.scaleLinear()
+                        .domain([0, 18])
+                        .range([height - margin, margin]); 
+
+frame.append("g")
+        .attr("transform", `translate(${margin}, 0)`)
+        .call(d3.axisLeft(linearScale)); 
+
+let bandScale = d3.scaleBand()
+                    .domain(frequency_data.map(function(d) { return d.key; }))
+                    .range([margin, width-margin])
+                    .paddingInner(0.05);
+
+frame.append("g")
+        .attr("transform", `translate(0, ${height-margin})`)
+        .call(d3.axisBottom(bandScale))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+
+
+//how to add color to single elements on click https://www.d3indepth.com/selections/
+//help with if statement structure to make color appear and disapear - https://stackoverflow.com/a/66241453 // Posted by Raycas 
+
+
+
+
+frame.selectAll("rect")
+    .data(frequency_data)
+    .join("rect")
+    .attr("x", function(d, i) {
+        return bandScale(d.key); 
+    })
+    .on('click', color_change)
+    .attr("y", function(d, i) {
+        return (linearScale(d.value));
+    })
+    .attr("width", bandScale.bandwidth())
+    .attr("height", function(d, i) {
+        return ((height - margin) -linearScale(d.value));
+    } ); 
+
+ function color_change(){
+    let rectangle = d3.select(this);
+      if(rectangle.style('fill') === 'black'){
+      rectangle.style('fill',  'pink');
+      } else {
+        rectangle.style('fill',  'black');
+  }
+ }
+
+// Found how to do from -> https://stackoverflow.com/q/14605348 -> Posted by DataByDavid
+
+    //Create Title 
+    frame.append("text")
+    .attr("x", width / 2 )
+    .attr("y", height /8)
+    .style("text-anchor", "middle")
+    .text("Frequency of family names across myData");
+
+    //Create X axis label   
+    frame.append("text")
+    .attr("x", width / 2 )
+    .attr("y",  height + margin)
+    .style("text-anchor", "middle")
+    .text("Family");
+
+    //Create Y axis label
+    frame.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 5)
+    .attr("x", -(height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Count");  
+
+
+
+
+//pie chart 
+// made with https://www.d3indepth.com/shapes/
+let frame_pie = d3.select("#pie_vis")
+                .append("svg")
+                .attr("width", width + margin*2) //multipled bc labels werent fitting
+                .attr("height", height + margin*2);
+
+
+frame_pie.append("g")
+        .attr("transform", `translate(${margin}, 0)`)
+
+
+frame_pie.append("text")
+.attr("x", width / 2 )
+.attr("y", height /8)
+.style("text-anchor", "middle")
+.text("Proportion of family names across myData");
+
+
+let pieGenerator = d3.pie()
+    .value(function(d) {return d.value;})
+    .sort(function(a, b) {
+        return a.key.localeCompare(b.key);
+    });
+
+// Create an arc generator with configuration
+let arcGenerator = d3.arc()
+    .innerRadius(20)
+    .outerRadius(100);
+
+let arcData = pieGenerator(frequency_data);
+
+
+ 
+ function click_pie(){
+    let prop = d3.select(this)
+    prop.append("text")
+    prop.text(d.data.key);
+ }
+
+
+
+frame_pie.selectAll('path')
+.data(arcData)
+.join('path')
+.on('click', click_pie)
+.attr('d', arcGenerator);
+
+
